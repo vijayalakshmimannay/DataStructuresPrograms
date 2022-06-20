c02ea5df6fa84432c1cdf30ac388b19111765bb3
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 17:38:36 +0530

    [vijayalakshmi] unordered list and LinkedList

---
+++ UnOrderedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnOrderedList
+{
+    internal class Node
+    {
+        public string data;
+        public Node next;
+        public Node(string data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ UnOrderedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace UnOrderedList
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedListData list = new LinkedListData();
+            list.Add("vijaya");
+            list.Add("lakshmi");
+            list.Add("harsha");
+            //list.DisPlay();
+            int a = list.Search("arjun");
+            Console.WriteLine("index position of element is: " + a);
+            list.InsertAtPerticularPosition(a + 1, "satya");
+            // list.DisPlay();
+            list.RemoveFirstNode("vijaya");
+            // list.RemoveLastNode("malli");
+            list.DisPlay();
+        }
+    }
+}
